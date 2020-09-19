48550ffb34f502550f3d7e2387bdc0a0289e0a24
Chikaia Dev <30716319+ChikaiaDev@users.noreply.github.com>
2020-09-19 13:20:08 +0300

    admin dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,24 @@ const router = express.Router();
 const app = express();
 const Job = require("./../models/job");
 
-router.get("/", (req, res) => {
-
-    res.render("admin/index");
+// jobs section
+router.get("/", async (req, res) => {
+  const jobs = await Job.find().sort({
+    createdAt: "desc",
+  });
+  res.render("admin/index", {
+    jobs: jobs,
+  });
 });
+router.get("/jobs/:slug", async (req, res) => {
+  const job = await Job.findOne({
+    slug: req.params.slug,
+  });
 
-
+  if (job == null) res.redirect("/");
+  res.render("admin/jobs/show", {
+    job: job,
+  });
+});
 
 module.exports = router;
